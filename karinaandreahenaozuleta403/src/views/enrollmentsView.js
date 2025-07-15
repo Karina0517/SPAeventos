893261202ab05dd12666e172743dbad1d58747f0
Karina0517 <henaokarina17@gmail.com>
2025-07-14 21:37:29 -0500

    actualizado enrollmentsView.js

--- karinaandreahenaozuleta403/src/views/enrollmentsView.js
+++ karinaandreahenaozuleta403/src/views/enrollmentsView.js
@@ -8,8 +8,6 @@ export function enrollmentsView() {
     <div class="container-fluid">
       <div class="row min-vh-100">
         <div class="col-md-3 col-lg-2 bg-light border-end d-flex flex-column align-items-center py-4">
-          <img src="avatar.jpg" class="rounded-circle mb-2" width="80" height="80" alt="Avatar">
-          <h6>Random guy</h6>
           <small class="text-muted mb-3">Visitante</small>
           <a href="#/dashboard/enrollments" class="btn btn-outline-primary w-75 mb-2">Mis inscripciones</a>
           <a href="#/dashboard/events-view" class="btn btn-outline-secondary w-75 mb-2">Eventos</a>
